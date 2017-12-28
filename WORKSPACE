cd7497faa466a006a557866dfb9c8ee1d8236eb0
Matt Wilson <wilson@cfht.hawaii.edu>
2017-12-27 17:51:46 -1000

    App now can generate database.
    However, flyway script is not invoking it.

---
+++ WORKSPACE
@@ -0,0 +1,11 @@
+# Will eventually allow me to put workspaces in sqldatabase with proper name.
+# Rules internal to sqldatabase should use @sqldatabase instead of //sqldatabase/
+new_local_repository(name = "sqldatabase", path = "./sqldatabase", build_file="//sqldatabase:BUILD")
+load("@sqldatabase//:defs.bzl", "sqldatabase_repositories")
+sqldatabase_repositories()
+
+# Will eventually allow me to put workspaces in flyway with proper name.
+# Rules internal to flyway should use @flyway instead of //flyway/
+new_local_repository(name = "flyway", path = "./flyway", build_file="//flyway:BUILD")
+load("@flyway//:defs.bzl", "flyway_repositories")
+flyway_repositories()
--- sqldatabase/create/bazeljooq/sqldatabase/CreateMysqlDatabase.java
+++ sqldatabase/create/bazeljooq/sqldatabase/CreateMysqlDatabase.java
@@ -1,24 +1,63 @@
 package bazeljooq.sqldatabase;
 
+import com.mysql.cj.jdbc.Driver;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateMysqlDatabase {
 
     // write true to outfile for now
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         // Must be, host, port, username, password
-        if (args.length != 5) {
-            System.out.println("Usage: <script> host port username password out-file");
+        if (args.length != 6) {
+            System.out.println("Usage: <script> host port username password database out-file");
             System.exit(-1);
         }
 
-        System.out.println(String.format("success %s", Arrays.asList(args)));
-        System.out.println(args[4]);
+        new Driver();
+        String host = args[0];
+        String port = args[1];
+        String username = args[2];
+        String password = args[3];
+        String dbname = args[4];
+
+        String url = String.format("jdbc:mysql://%s:%s", host, port);
+        Connection conn = null;
+        Statement statement = null;
+        boolean success = false;
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            statement = conn.createStatement();
+            statement.executeUpdate(String.format("CREATE DATABASE IF NOT EXISTS %s", dbname));
+            success = true;
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ignore) {
+                }
+            }
+
+            if (conn != null) {
+                 try {
+                    conn.close();
+                } catch (SQLException ignore) {
+                }
+            }
+        }
+
         BufferedWriter file = Files.newBufferedWriter(Paths.get(args[4]));
+        if (!success) {
+            System.out.println("Could not create database");
+            System.exit(-1);
+        }
+
         file.write("true");
     }
 }
