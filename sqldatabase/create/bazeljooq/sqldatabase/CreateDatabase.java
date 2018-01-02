d8109684892250080e2a265a326a3ca3431c19da
Matt Wilson <wilson@cfht.hawaii.edu>
2018-01-02 12:23:03 -1000

    Make tool changes cause re-build of all dependencies.

--- sqldatabase/create/bazeljooq/sqldatabase/CreateDatabase.java
+++ sqldatabase/create/bazeljooq/sqldatabase/CreateDatabase.java
@@ -2,8 +2,11 @@ package bazeljooq.sqldatabase;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,12 +14,52 @@ import java.sql.Statement;
 
 public class CreateDatabase {
 
+    // From: https://memorynotfound.com/calculate-file-checksum-java/
+    public enum Hash {
+        SHA256("SHA-256");
+
+        private String name;
+
+        Hash(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String checksum(InputStream in) {
+            try {
+                MessageDigest digest = MessageDigest.getInstance(getName());
+                byte[] block = new byte[4096];
+                int length;
+                while ((length = in.read(block)) > 0) {
+                    digest.update(block, 0, length);
+                }
+                return bytesToHex(digest.digest());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // from: https://stackoverflow.com/questions/15429257/how-to-convert-byte-array-to-hexstring-in-java
+        private String bytesToHex(byte[] in) {
+            final StringBuilder builder = new StringBuilder();
+            for(byte b : in) {
+                builder.append(String.format("%02x", b));
+            }
+            return builder.toString();
+        }
+    }
+
+
     // write true to outfile for now
     public static void main(String[] args)
             throws IOException, SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
         // Must be, host, port, username, password
         if (args.length != 6) {
-            System.out.println("Usage: <script> jdbc-connection-string username password driver-class database out-file");
+            System.out
+                    .println("Usage: <script> jdbc-connection-string username password driver-class database out-file");
             System.exit(-1);
         }
 
@@ -48,7 +91,7 @@ public class CreateDatabase {
             }
 
             if (conn != null) {
-                 try {
+                try {
                     conn.close();
                 } catch (SQLException ignore) {
                 }
@@ -60,7 +103,11 @@ public class CreateDatabase {
             System.out.println("Could not create database");
             System.exit(-1);
         }
-        file.write("true");
+        String toolSha = Hash.SHA256.checksum(Files.newInputStream(
+                Paths.get(CreateDatabase.class.getProtectionDomain().getCodeSource().getLocation().getPath()),
+                StandardOpenOption.READ));
+
+        file.write(toolSha);
         file.close();
     }
 }
